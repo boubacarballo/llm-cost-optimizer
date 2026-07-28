320668f0e4143d0a49a80fe93a00d2be2abd5d51
Boubacar Ballo <bab9755@nyu.edu>
2026-07-28 19:11:34 +0000

    use server in main file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from chat import Chat
 import sys
-
+import requests
+import os
 chat = Chat()
 
 def send_request(prompt: str, model_config):
@@ -17,6 +18,10 @@ def send_request(prompt: str, model_config):
     
 if __name__ == "__main__":
     
+    prompt_classifier_url = os.getenv("PROMPT_CLASSIFIER_SERVER_URL")
+    
+    if not prompt_classifier_url:
+        raise "Unable to ge prompt classifier URL"
     
     while True: # each full loop corresponds to one session
         
@@ -27,6 +32,21 @@ if __name__ == "__main__":
             break
         
         # TODO: logic to determine model and provider
+        
+        # get the complexity here
+        params = {
+            "prompt": prompt,
+        }
+        response = requests.post(
+            url=prompt_classifier_url,
+            json=params
+        )
+        
+        result = response.json()
+        
+        print(result)
+        
+        exit(1)
         
         provider = "anthropic"
         model = "claude-haiku-4-5-20251001"
