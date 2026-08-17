c496e7915a4e9e8871fc2df18f9fbf11316ea8d8
Boubacar Ballo <bab9755@nyu.edu>
2026-08-17 22:38:08 +0000

    update async worker

--- services/api/src/llm_cost_optimizer/async_worker.py
+++ services/api/src/llm_cost_optimizer/async_worker.py
@@ -3,7 +3,30 @@ from arq import run_worker, create_pool
 from arq.connections import RedisSettings
 from llm_cost_optimizer.utils import TaskType
 from llm_cost_optimizer.verifiers.summarization import verify_summarization
+from llm_cost_optimizer.verifiers.rewrite import verify_rewrite
 REDIS_SETTINGS = RedisSettings()
+
+async def verify_response(
+    task_type: TaskType,
+    prompt: str,
+    response: str,
+    model_config: dict
+):
+    
+    response = None
+    if task_type.value == "Rewrite":
+            
+        response = await verify_rewrite(
+            prompt=prompt,
+            response=response,
+            judge_model_config=model_config
+        )
+        
+        
+    
+    return response
+        
+    
 
 async def handle_verification(
     ctx, 
@@ -13,11 +36,37 @@ async def handle_verification(
     model_config: dict
     
 ):
-    match task_type:
-        case TaskType.SUMMARIZATION:
-            summarization_result = await verify_summarization(prompt, response, model_config)
-            score = 0.5 * summarization_result.precision + 0.5 * summarization_result.recall
+
+    response = await verify_response(
+        task_type=task_type,
+        prompt=prompt,
+        response=response,
+        model_config=model_config
+        
+    )
+    
+    if not response:
+        raise Exception
+    
+    if response.verdict == "Bad":
+        pass
+        alternate_models = get_alternate_models(model_config)
+        
+        for cfg in alternate_models:
+            #async run for each
+        #if its bad, then its a ROUTING FAILURE , we take a model that's 1 tier higher (if 1 then 2, if 2 then 3, if 3 then we take 3 but another provider's highest tier model)
+            # we also take another provider of the same tier
             
+            #run the same prompt with that "higher-tier" model (this we will make async so its fast)
+            
+            #run the prompt-response pair through the verifier again
+            
+            # take the 3-4 prompt-response pairs and make then go through judge and pick the best and cheapest
+            #LOG the routing failure so we can retrain the model down the line
+        
+    else: #when the response is good
+        
+        pass
         
             
 
