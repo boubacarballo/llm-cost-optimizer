41b13054573ad2685bcb54993c5884ba65cc98e8
Boubacar Ballo <bab9755@nyu.edu>
2026-08-19 03:29:18 +0000

    add requests to both task- and prompt-classifier

--- services/api/src/llm_cost_optimizer/main.py
+++ services/api/src/llm_cost_optimizer/main.py
@@ -1,5 +1,5 @@
 from llm_cost_optimizer.chat import Chat, Response
-from llm_cost_optimizer.utils import TaskType, load_models_configs, breakdown_results, get_candidate_models, select_model_and_provider, compute_request_cost, hash_text
+from llm_cost_optimizer.utils import TaskType, load_models_configs, breakdown_results, get_candidate_models, select_model_and_provider, compute_request_cost, hash_text, get_model
 import sys
 import requests
 import os
@@ -24,6 +24,7 @@ async def lifespan(app: FastAPI):
     redis = await create_pool(RedisSettings())
     global prompt_classifier_url
     global model_configs_path
+    task_classifier_url = os.getenv("TASK_CLASSIFIER_SERVER_URL")
     prompt_classifier_url = os.getenv("PROMPT_CLASSIFIER_SERVER_URL")
     model_configs_path = os.getenv("MODEL_CONFIGS_PATH")
     global config
@@ -55,43 +56,50 @@ class ChatRequest(BaseModel):
     
 @app.post("/chat")
 async def chat(req: ChatRequest, session: SessionDep):
-    start = time.perf_counter()
     
     params = {
         "prompt": req.messages[-1].content,
     }
-    response = requests.post(
-        url=prompt_classifier_url,
+    response = await requests.post(
+        url=task_classifier_url,
         json=params
     )
     result = response.json()
-    tier_flags, contextual_knowledge, complexity = breakdown_results(result)
-    candidate_models = get_candidate_models(tier_flags, config)
-
-        
-    model_config = select_model_and_provider(candidate_models, context_window=12700)
-    response = await chat.send_request(
-        messages=req.messages,
-        model_config=model_config,
-    )
     task_type = TaskType(result["task_type_1"][0])
-    response.cost = compute_request_cost(model_config, response.input_tokens, response.output_tokens)
+    res = await requests.post(
+        url=prompt_classifier_url,
+        json={
+            "prompt": req.messages[-1].content,
+            "token_count": len(req.messages[-1].content) // 4,
+            "context_provided": True if len(req.messages) else False,
+            "task_type": task_type.value
+            "device": "auto"
+        }
+    )
+    data = res.json()
+    model_config = get_model(
+        data=data,
+        context_window=120000
+    )
+    model_response = await chat.send_request(
+        messages=req.messages,
+        model_config=model_config
+    )
+    
+    model_response.cost = compute_request_cost(model_config, model_response.input_tokens, model_response.output_tokens)
     request_event = RequestEvent(
             prompt_hash=hash_text(req.messages[-1].content),
             model=model_config["id"],
             provider=model_config["provider"],
-            cost=response.cost,
-            latency=0.0,
-            escalated=False,    
+            cost=model_response.cost,
+            latency=0.0, 
     )
+    
     session.add(request_event)
     await session.commit()
     await session.refresh(request_event)
-    await redis.enqueue_job('handle_verification', task_type, req.messages[-1].content, response.output_text, model_config)
+    await redis.enqueue_job('handle_verification', task_type, req.messages[-1].content, model_response.output_text, model_config)
     return response
-    
-    
-    
     
 def start():
     uvicorn.run("llm_cost_optimizer.main:app", host="127.0.0.1", port=8080, reload=True)
